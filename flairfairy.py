922cc949792dd6c5c10498db774a1483a1cbc842
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2012-08-05 21:18:18 +0100

    Merged languageflair into flairfairy

--- flairfairy.py
+++ flairfairy.py
@@ -1,11 +1,16 @@
 ## Parse Command Line Argument ##
 import logging # Needed for the logging command line argument default
+import os
+import re
 import reddit
 import sys
 import time
 from optparse import OptionParser, make_option
 
-import languageflair
+import requests
+
+import proxies
+from settings import code_sites, name_dict, flair_templates
 
 option_list = [
     make_option("-u", "--username", dest = "username", type = str,
@@ -66,6 +71,8 @@ parser = OptionParser(
     option_list = option_list
 )
 
+log = logging.getLogger("flairfairy.languageflair")
+
 # Run the parser
 options = parser.parse_args()[0]
 
@@ -99,10 +106,77 @@ except reddit.errors.InvalidUserPass:
     
 log.info("Succesfully logged in as user %s." % options.username)
 
+def make_shortname(long_name):
+    for regex, shortname in name_dict.iteritems():
+        if re.match(regex, long_name, re.IGNORECASE) != None:
+            return shortname
+    return long_name
+
+def find_template(language):
+    for i in flair_templates:
+        if i["name"] == language:
+            return i
+    return None
+
+proxy = proxies.NewSubmissionsProxy(options.subreddit)
+
+def run(reddit, options):
+    log.debug("Started work cycle.")
+    
+    for i in proxy.get(reddit):
+        # Create a human readable id for the post to use in our log
+        # messages
+        post_id = "\"%s\" (%s)" % (i.title, i.id)
+        
+        log.debug("Processing post %s." % post_id)
+        
+        # Ignore all submissions that already have flair
+        if i.link_flair_text and not options.blow_away:
+            continue
+          
+        # Don't know how to parse code from this domain
+        if not i.domain in code_sites.keys():
+            continue
+        page = requests.get(i.url)
+        if not page.ok:
+            log.debug("Got a %d error when opening %s" % (page.status_code,
+                                                          i.url))
+            continue
+        language = re.search(code_sites[i.domain], page.content, 
+                   re.DOTALL | re.IGNORECASE)
+        if language is None:
+            log.info("Language could not be determined for post %s."
+                         % post_id)
+            continue
+        language = language.groups()[0].lower()
+        shortname = make_shortname(language)
+        
+        log.debug("Determined language to be %s for post %s."
+                     % (shortname, post_id))
+        
+        # Map the name to a flair template
+        flair_template = find_template(shortname)
+        if not flair_template:
+            log.info("Determined language \"%s\" for post %s could not be "
+                     "matched to any flair template."
+                         % (shortname, post_id))
+            continue
+            
+        # Set the flair if we're not in debug mode
+        if not options.debug:
+            i.set_flair(
+                flair_text = flair_template["name"],
+                flair_css_class = flair_template["css"]
+            )
+            
+            log.info("Set language of post %s to %s" % (post_id, shortname))
+            log.debug("Full flair: " + str(flair_template))
+            
+    log.debug("Work cycle finished.")
+
 ## Do it ##
-routines = languageflair.LanguageFlair(options, options.subreddit)
 while True:
-    routines.run(r, options)
+    run(r, options)
     time.sleep(options.refresh_speed)
 
 log.info("Exiting...")
